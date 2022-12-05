1f76b7b0b20403db986afe8c291a8fb5560236e1
Oleksandr Prydatko <o_prydatko@ukr.net>
2022-12-05 11:55:51 +0200

    виправили недоліки

--- One.java
+++ One.java
@@ -5,22 +5,15 @@ public class One {
 	private int a;
 	int b;
 	int temp;
-<<<<<<< HEAD
 	int c;
 	
 	public One(int a, int b, int temp, int c) {
-=======
 	
-	public One(int a, int b, int temp) {
->>>>>>> 0be4b4952bc717e362091bdd614258523d250586
 		super();
 		this.a = a;
 		this.b = b;
 		this.temp = temp;
-<<<<<<< HEAD
 		this.c = c;
-=======
->>>>>>> 0be4b4952bc717e362091bdd614258523d250586
 	}
 
 	int sumaDemo() {
