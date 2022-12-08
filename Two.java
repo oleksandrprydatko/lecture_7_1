ac262a4bc7b0e41b3143c649d1ef64a3c479f918
Oleksandr Prydatko <o_prydatko@ukr.net>
2022-12-08 11:56:01 +0200

    попрацював 2 юзер

---
+++ Two.java
@@ -0,0 +1,18 @@
+public class One {
+
+	int a;
+	int b;
+	int temp;
+	
+	public One(int a, int b, int temp) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.temp = temp;
+	}
+
+	int suma() {
+		int c = a + b;
+		return c;
+	}
+}
